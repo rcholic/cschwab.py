a7719c214381e58bed3f0dd7e15f7ae25d25df48
rcholic <ivytony@gmail.com>
2024-06-04 19:48:27 -0700

    github actions

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md") as file:
+    long_description = file.read()
+
+setup(
+    name="CSchwabPy",
+    version="0.1.0",
+    description="Charles Schwab Stock & Option Trade API Client for Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Tony W",
+    author_email="ivytony@gmail.com",
+    url="https://pypi.org/project/CSchwabPy/",
+    keywords=["python", "CSchwabPy", "Charles Schwab", "Stock", "Option", "Trade"],
+)
